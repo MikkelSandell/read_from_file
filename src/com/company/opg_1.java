c74d9879315a24191b20cb99c0385324dd321d5c
Bruger <mikk00j3@stud.kea.dk>
2021-10-20 11:46:05 +0200

    forst commit

---
+++ src/com/company/opg_1.java
@@ -0,0 +1,36 @@
+package com.company;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Locale;
+import java.util.Scanner;
+
+public class opg_1 {
+
+    public static void main(String[] args) {
+        File file =new File("data/demo.txt");
+        try {
+            Scanner sc = new Scanner(file);
+            String linje = sc.nextLine();
+            String[] ordListe = linje.split(" ");
+            for (int i = 0; i < ordListe.length; i++) {
+                if (ordListe[i].length() > 5){
+                    System.out.print(ordListe[i].toUpperCase() + " ");
+                }else {
+                    System.out.print(ordListe[i]+ " ");
+                }
+            }
+            linje = sc.nextLine();
+            String[] ordListe2 = linje.split(" ");
+            for (int i = 0; i < ordListe2.length; i++) {
+                if (ordListe2[i].length() > 5){
+                    System.out.print(ordListe2[i].toUpperCase()+ " ");
+                }else {
+                    System.out.print(ordListe2[i]+ " ");
+                }
+            }
+        } catch (FileNotFoundException e) {
+            System.out.println("Kaffepause " + e.getMessage());
+        }
+    }
+}
---
+++ src/com/company/opg_2.java
@@ -0,0 +1,49 @@
+package com.company;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class opg_2 {
+    public static void main(String[] args) {
+        File file =new File("data/DMI.txt");
+        try {
+            Scanner sc = new Scanner(file);
+            String linje1 = sc.nextLine();
+            String[] ordListe = linje1.split(" ");
+            int int1 = Integer.parseInt(ordListe[1]);
+
+            String linje2 = sc.nextLine();
+            String[] ordListe2 = linje2.split(" ");
+            int int2 = Integer.parseInt(ordListe2[1]);
+
+            String linje3 = sc.nextLine();
+            String[] ordListe3 = linje3.split(" ");
+            int int3 = Integer.parseInt(ordListe3[1]);
+
+            String linje4 = sc.nextLine();
+            String[] ordListe4 = linje4.split(" ");
+            int int4 = Integer.parseInt(ordListe[1]);
+
+            String linje5 = sc.nextLine();
+            String[] ordListe5 = linje5.split(" ");
+            int int5 = Integer.parseInt(ordListe5[1]);
+
+            String linje6 = sc.nextLine();
+            String[] ordListe6 = linje6.split(" ");
+            int int6 = Integer.parseInt(ordListe6[1]);
+
+            String linje7 = sc.nextLine();
+            String[] ordListe7 = linje7.split(" ");
+            int int7 = Integer.parseInt(ordListe7[1]);
+
+            int total = int1+int2+int3+int4+int5+int6+int7;
+            int gennemsnit = total/7;
+            System.out.println("gennemsnit temperaturen er " + gennemsnit);
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+
+}
